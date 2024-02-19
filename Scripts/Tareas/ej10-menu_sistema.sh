8ea7d9096f19f9b3558117944e77fe5ceaa26007
GRMiguelAngel <miguelangel2e2005@gmail.com>
2024-02-20 00:00:20 +0100

    Subida de ejercicio BASH

--- Scripts/Tareas/ej10-menu_sistema.sh
+++ Scripts/Tareas/ej10-menu_sistema.sh
@@ -1,31 +1,53 @@
 clear
-echo "************"
-echo "*** MENÚ ***"
-echo "************"
+echo "     ************"
+echo "     *** MENÚ ***"
+echo "     ************"
 echo ""
-echo "Porlibre: Mostrar el espacio libre de la partición raíz(%)"
-echo "Tamlibre: Mostrar el espacio libre de la partición raíz(tamaño)"
-echo "Usuario: Indica el usuario actual"
-echo "Maquina: Mostrar el nombre de la máquina"
-echo "Usuarios: Mostrar número de usuarios en el sistema"
-echo "Espacio: Mostrar espacio total usado"
-echo ""
+echo "porlibre: Espacio libre de la partición raíz (en porcentaje)."
+echo "tamlibre: Espacio libre de la partición raíz (en tamaño)."
+echo "usuario: Usuario actual."
+echo "maquina: Nombre de la máquina."
+echo "usuarios: Número de usuarios del sistema."
+echo "espacio: Espacio usado en todos los directorios."
+echo "directorios: Número de directorios en el directorio raíz." 
+echo "salir: Salir del sistema."  
 read -p "Introduzca su opción: " opcion
 
 case "$opcion" in
 
-    Porlibre|porlibre|PORLIBRE|1) ls -h
+    porlibre|1) porcentaje_libre=$(df -h)
+    echo "El porcentaje de disco libre es $porcentaje_libre"
     ;;
 
-    b|B|2) df -h
+    tamlibre|2)tamaño_libre=$(df -h)
+    echo "El espacio del disco libre es $tamaño_libre"
+    ;;
+
+    usuario|3) usuario=$(whoami)
+    echo "Usuario: $usuario"
     ;;
     
-    c|C|3) 
-    read -p "Introduzca nombre del fichero: " nombre_fich
-    stat -c "%a %A %n" $nombre_fich
+    maquina|4) 
+    maquina=$(hostname) 
+    echo "El nombre de la máquina es $maquina"
     ;;
 
-    d|D|4) free
+    usuarios|5)
+    usuarios=$(who | cut -d " " -f 1 | sort -u | wc -l)
+    echo "El total de usuarios de esta máquina es $usuarios"
+    ;;
+
+    espacio|6)
+    espacio_usado=$(sudo du -sh /home/* | cut -f1) 
+    echo "El espacio usado de directorios personales es $espacio_usado"
+    ;;
+
+    directorios|7)
+    directorios=$(ls -R / 2> /dev/null | wc -l)
+    echo "Hay $directorios directorios en el directorio raíz."
+    ;;
+
+    salir|8)
     ;;
 
     *) echo "ERROR: '$opcion' no es una opción válida."
